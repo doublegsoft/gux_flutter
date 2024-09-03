01a7b9f1f047383754d453c279a1faa38d227b1d
Christian Gann <guo.guo.gan@gmail.com>
2024-09-03 23:09:14 +0800

    completed grid view

--- lib/widget/gx_grid_view.dart
+++ lib/widget/gx_grid_view.dart
@@ -21,15 +21,21 @@ import 'package:image_picker/image_picker.dart';
 import 'package:intl/intl.dart';
 import 'package:photo_view/photo_view.dart';
 
-class AlwaysDisabledFocusNode extends FocusNode {
-  @override
-  bool get hasFocus => false;
-}
+import 'gx_widget_size.dart';
+
+typedef NullableColumIndexedWidgetBuilder =
+GXWidgetSize Function(BuildContext context, Map<String, dynamic> item, int columnIndex);
 
 class GXGridView extends StatefulWidget {
 
+  final List<Map<String, dynamic>> data;
+
+  final NullableColumIndexedWidgetBuilder itemBuilder;
+
   const GXGridView({
     Key? key,
+    required this.data,
+    required NullableColumIndexedWidgetBuilder this.itemBuilder,
   }) : super(key: key);
 
   @override
@@ -38,79 +44,99 @@ class GXGridView extends StatefulWidget {
 
 class GXGridViewState extends State<GXGridView> {
 
-  final Map<String, dynamic> _values = {};
+  final ScrollController _scrollController = ScrollController();
 
-  final Map<String, TextEditingController> _controllers = {};
+  bool _loading = false;
+
+  List<Map<String,dynamic>> _data = [];
+
+  int _firstColumnHeight = 0;
+
+  int _secondColumnHeight = 0;
 
   @override
   void initState() {
     super.initState();
-
+    _scrollController.addListener(_scrollListener);
+    _data = widget.data;
   }
 
   @override
   void dispose() {
-    _controllers.forEach((key, value) {
-      value.dispose();
-    });
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    List<Widget> cols = buildColumns();
     return SingleChildScrollView(
+      controller: _scrollController,
       child: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Expanded(
-              flex: 1, // Equal flex value for both columns
-              child: Column(
-                children: [
-                  Container(
-                    height: 200,
-                    color: Colors.red,
-                  ),
-                ],
-              ),
-            ),
+            cols[0],
             SizedBox(width: 8),
-            Expanded(
-              flex: 1, // Equal flex value for both columns
-              child: Column(
-                children: [
-                  Container(
-                    height: 200,
-                    color: Colors.blue,
-                  ),
-                  Container(
-                    height: 200,
-                    color: Colors.blue,
-                  ),
-                  Container(
-                    height: 200,
-                    color: Colors.blue,
-                  ),
-                  Container(
-                    height: 200,
-                    color: Colors.blue,
-                  ),
-                  Container(
-                    height: 200,
-                    color: Colors.blue,
-                  ),
-                  Container(
-                    height: 200,
-                    color: Colors.blue,
-                  ),
-                ],
-              ),
-            ),
+            cols[1],
           ],
         ),
       ),
     );
   }
 
+  // Size getWidgetSize() {
+  //   final RenderBox renderBox = _itemKey.currentContext!.findRenderObject() as RenderBox;
+  //   final size = renderBox.size;
+  //   final position = renderBox.localToGlobal(Offset.zero);
+  //   print('Size: ${size.width} x ${size.height}');
+  //   print('Position: ${position.dx}, ${position.dy}');
+  //   return size;
+  // }
+
+  void _scrollListener() {
+    if (_scrollController.position.pixels == _scrollController.position.maxScrollExtent) {
+      _data.addAll([{},{},{},{}]);
+      setState((){});
+    }
+  }
+
+  List<Widget> buildColumns() {
+    _firstColumnHeight = 0;
+    _secondColumnHeight = 0;
+    List<Widget> ret = [];
+    List<Widget> widgetsInFirstCol = [];
+    List<Widget> widgetsInSecondCol = [];
+    for (int i = 0; i < _data.length; i++) {
+      Map<String,dynamic> item = _data[i];
+      GXWidgetSize widgetSize = widget.itemBuilder(context, item, i % 2);
+      if (_firstColumnHeight <= _secondColumnHeight) {
+        widgetsInFirstCol.add(widgetSize.child);
+        widgetsInFirstCol.add(SizedBox(height: 8));
+        _firstColumnHeight += 8 + widgetSize.height;
+      } else {
+        widgetsInSecondCol.add(widgetSize.child);
+        widgetsInSecondCol.add(SizedBox(height: 8));
+        _secondColumnHeight += 8 + widgetSize.height;
+      }
+    }
+
+    Widget firstColumn = Expanded(
+      flex: 1,
+      child: Column(
+        children: widgetsInFirstCol,
+      ),
+    );
+
+    Widget secondColumn = Expanded(
+      flex: 1,
+      child: Column(
+        children: widgetsInSecondCol,
+      ),
+    );
+
+    ret.add(firstColumn);
+    ret.add(secondColumn);
+    return ret;
+  }
 }
--- lib/widget/gx_widget_size.dart
+++ lib/widget/gx_widget_size.dart
@@ -14,70 +14,49 @@
 ** ──────────────────────────────────────────────────
 */
 import 'package:flutter/material.dart';
-import 'package:intl/date_symbol_data_local.dart';
-import 'package:table_calendar/table_calendar.dart';
+import 'package:flutter/scheduler.dart';
 
-import 'package:gux/widget/gx_grid_view.dart';
-import 'package:gux/widget/gx_widget_size.dart';
+class GXWidgetSize extends StatefulWidget {
 
-class GridViewPage extends StatefulWidget {
+  final Widget child;
+
+  final Function onChange;
+
+  final int height;
+
+  const GXWidgetSize({
+    Key? key,
+    required this.onChange,
+    required this.child,
+    required this.height,
+  }) : super(key: key);
+
   @override
-  GridViewState createState() => GridViewState();
+  GXWidgetSizeState createState() => GXWidgetSizeState();
 }
 
-class GridViewState extends State<GridViewPage> {
-
-  List<Map<String, dynamic>> data = [{},{},{}];
-
+class GXWidgetSizeState extends State<GXWidgetSize> {
   @override
   Widget build(BuildContext context) {
-    data = [{},{},{},{},{},{},{}];
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('栅格列表'),
-      ),
-      body: GXGridView(
-        data: data,
-        itemBuilder: (context, item, columnIndex) {
-          return _buildCard(columnIndex);
-        },
-      ),
+    SchedulerBinding.instance.addPostFrameCallback(postFrameCallback);
+    return Container(
+      key: widgetKey,
+      child: widget.child,
     );
   }
 
-  GXWidgetSize _buildCard(int columnIndex) {
-    return GXWidgetSize(
-      child: Container(
-        padding: EdgeInsets.only(bottom: 8.0),
-        height: columnIndex == 0 ? 220 : 320,
-        color: Color.fromRGBO(234,234,234,1),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              width: MediaQuery.of(context).size.width,
-              height: 160,
-              child: FittedBox(
-                fit: BoxFit.fill,
-                child: Image.network('https://picsum.photos/300/160'),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical:8,horizontal:16),
-              child: Text("hello world", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-            ),
-            (columnIndex == 0 ? Container() : Expanded(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(vertical:8,horizontal:16),
-                  child: Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. when an unknown printer took a galley of type and scrambled it to make a type specimen book.", style: TextStyle(fontSize: 12)),
-                ),
-              )
-            ),
-          ]
-        )
-      ),
-      height: columnIndex == 0 ? 220 : 320,
-      onChange: (size) {},
-    );
+  var widgetKey = GlobalKey();
+
+  var oldSize;
+
+  void postFrameCallback(_) {
+    var context = widgetKey.currentContext;
+    if (context == null) return;
+
+    var newSize = context.size;
+    if (oldSize == newSize) return;
+
+    oldSize = newSize;
+    widget.onChange(newSize);
   }
 }
